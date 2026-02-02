cddeb3ee7f8df93b37e61b14f27186596bc08526
Alfonso <espinozalfonsoch@gmail.com>
2026-02-01 19:08:40 -0500

    suma
    asuma

--- database/db.py
+++ database/db.py
@@ -6,17 +6,15 @@ from psycopg2 import Error
 def get_db():
     try:
         connection = psycopg2.connect(
-            host="localhost",        # 🔴 LOCAL
-            port="5433",
-            user="postgres",         # o el usuario que creaste
-            password="1234",
-            dbname="chatbot_secretaria",
-            sslmode="disable"        # 🔴 LOCAL → SIN SSL
+            host="dpg-d5sm0dc9c44c739csnhg-a.oregon-postgres.render.com",
+            port="5432",
+            user="admin",
+            password="x9Hw2ZP0vV0LN74kWwSXufApbqhbO2nO",
+            dbname="chatbot_db_z5tb",
+            sslmode="require"   # 🔑 ESTA LÍNEA
         )
         return connection
 
     except Error as e:
         print(f"Error al conectar a PostgreSQL: {e}")
         return None
-######################################################################################
-######################################################################################
